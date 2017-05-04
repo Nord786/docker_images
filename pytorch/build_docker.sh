5aec39258002a4aac0534f52e756f2ae25c81948
Andrey Murashev <a.murashev@reco-classimg-t2.go.devmail.ru>
2017-05-04 19:50:30 +0300

    Update for pytorch

--- pytorch/build_docker.sh
+++ pytorch/build_docker.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 rm -rf pytorch || true
+git clone https://github.com/pytorch/pytorch
 cat Dockerfile.append >> pytorch/Dockerfile
 cd pytorch
 docker build -t pytorch_murashev_new .
